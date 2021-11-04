f57aa723399c3fad9436c8980ae6585b932bd9cf
alProkopov <prokopov.vrn@gmail.com>
2021-11-04 16:19:37 +0300

    feat:add copy-directory

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,17 +5,13 @@ const options = { withFileTypes: true };
 
 fs.readdir(filePath, options, (err, files) => {
   files.forEach((item) => {
-    // const name = item.name.split(".").splice(0, 1);
     const itemExt = path.extname(item.name);
     const itemPath = path.join(filePath, item.name);
     const name = path.basename(itemPath, itemExt);
 
-    fs.stat(itemPath, (err, stats) => {
-
-    });
     if (item.isFile()) {
       fs.stat(itemPath, (err, stats) => {
-        console.log(`${name} ${itemExt} ${(stats.size/1024).toFixed(2)}kb`);
+        console.log(`${name} ${itemExt} ${(stats.size / 1024).toFixed(2)}kb`);
       });
     }
   });
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,39 @@
+const fs = require("fs");
+const path = require("path");
 
+const dirPath = path.join(__dirname, "files");
+const copyDirPath = path.join(__dirname, "copy-files");
+
+const options = { withFileTypes: true };
+console.log(copyDirPath);
+
+let folderExist = fs.access(copyDirPath, fs.constants.F_OK, (err) => {
+  if (err) {
+    createDir();
+    copyFiles();
+  } else {
+    removeDir(createDir);
+  }
+});
+
+function createDir() {
+  fs.mkdir(copyDirPath, { recursive: true }, () => {});
+  copyFiles();
+}
+
+function removeDir(callback) {
+  fs.rm(copyDirPath, { recursive: true }, (err) => {
+    if (err) throw err;
+    callback();
+  });
+}
+
+function copyFiles() {
+  fs.readdir(dirPath, options, (err, files) => {
+    files.forEach((item) => {
+      const filePath = path.join(dirPath, item.name);
+      const copyFilePath = path.join(copyDirPath, item.name);
+      fs.copyFile(filePath, copyFilePath, (err) => {});
+    });
+  });
+}
